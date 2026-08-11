3e90dc661863b054dce9c7b0dee3c23fd872002b
Mikael <mikaeltubetv@gmail.com>
2026-08-11 18:26:10 +0800

    final

--- ex00/Enemy.cpp
+++ ex00/Enemy.cpp
@@ -8,5 +8,5 @@ Enemy::Enemy(std::string name, int hp) : Entity(name)
 }
 
 std::string Enemy::describe() const {
-    return "Enemy " + m_name;
+    return "Enemy " + m_name + " " + "(hp " + std::to_string(m_hp) + ")";
 }
